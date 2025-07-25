9cdc65a4523483b86770ad70b37bbbea741eb670
kelvintony <kelvintonyph@gmail.com>
2025-07-25 17:58:22 +0100

    hhjhj

--- src/app/user/dashboard/Services/Services.jsx
+++ src/app/user/dashboard/Services/Services.jsx
@@ -52,7 +52,7 @@ const serviceData = [
     id: 5,
     title: 'Transactions',
     description: 'Track every transaction',
-    url: '/user/transactions/s',
+    url: '/user/transactions/',
     icon: <GiHistogram size={17} />,
     backgroundColor: '#FF9770',
   },
